d0e71edc0d43b80cc52667f0885f2276dc4b09ff
Flame <caua.ferroni.100@gmail.com>
2025-04-16 09:53:35 -0300

    Conserto BD

--- Projeto Integrador/conexeplo.py
+++ Projeto Integrador/conexeplo.py
@@ -1,5 +1,6 @@
 import mysql.connector
-
+import cifra_de_palavra
+import cifra_num_final
 def connect_to_database():
     try:
         connection = mysql.connector.connect(
@@ -15,7 +16,7 @@ def connect_to_database():
         print(f"Erro: {err}")
         return None
 
-def insert_data(consumoagua, LixoR, LixoT, consumoenergia, op):
+def insert_data(consumoagua, LixoR, LixoT, consumoenergia, op, data):
     conn = connect_to_database()
     cursor = conn.cursor()
     cursor.execute('''
@@ -24,13 +25,31 @@ def insert_data(consumoagua, LixoR, LixoT, consumoenergia, op):
             lixo_reciclavel FLOAT,
             lixo_total FLOAT,
             consumo_energia FLOAT,
-            op_veiculos VARCHAR(255)
+            op_veiculos VARCHAR(255),
+            data_entrada int not NULL
         )
     ''')
     cursor.execute('''
-        INSERT INTO dados_sustentavel (consumo_agua, lixo_reciclavel, lixo_total, consumo_energia, op_veiculos)
-        VALUES (%s, %s, %s, %s, %s)
-    ''', (consumoagua, LixoR, LixoT, consumoenergia, op))
+        INSERT INTO dados_sustentavel (consumo_agua, lixo_reciclavel, lixo_total, consumo_energia, op_veiculos, data_entrada)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    ''', (consumoagua, LixoR, LixoT, consumoenergia, op, data))
     conn.commit()
+    cursor.close()
+    conn.close()
+
+def fetch_data(data):
+    conn = connect_to_database()
+    if conn is None:
+        print("Não foi possível conectar ao banco de dados.")
+        return
+
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT * FROM dados_sustentavel where data_entrada = %s
+    ''', (data,))
+    rows = cursor.fetchall()
+    return rows
+
+    # Exibindo os dados retornados
     cursor.close()
     conn.close()
--- Projeto Integrador/projeto.py
+++ Projeto Integrador/projeto.py
@@ -3,6 +3,7 @@ from conexeplo import *
 from cifra_de_palavra import *
 from cifra_num_final import *
 controle = "S"
+escolha = ""
 #Conexão do banco de dados
 def main():
     connection = connect_to_database()
@@ -10,131 +11,119 @@ def main():
         connection.close()
 #repetição do teste até o fim do usuário
 while controle == "S":
-    if __name__ == "__main__":
-        main()
+    escolha = input("Escolha uma opção:\n1 - Cadastrar\n2 - Consultar")
+    while escolha != "1" and escolha != "2":
+        escolha = input("Valor inválido, escolha 1 ou 2: ")
+    if escolha == "1":
+        print("Cadastro")
+        if __name__ == "__main__":
+            main()
 #Entrada de data do usuário e validação do dado
-    try:
-        data = (input("Informe a data de hoje: "))
-        while int(data) > 31:
+        try:
+            data = (input("Informe a data de hoje: "))
+            while int(data) > 31:
+             data = input("Data inválida, insira uma data válida: ")
+        except ValueError:
             data = input("Data inválida, insira uma data válida: ")
-    except ValueError:
-        data = input("Data inválida, insira uma data válida: ")
-    dataC = cifra_hill(data, key_matrix)
+        dataC = cifra_hill(data, key_matrix)
     #Entrada de consumo de água e validação do dado
-    try:
-        consumoagua = float(input("Informe seu consumo de água informado na conta de água(Litros/Dia): "))
-        while consumoagua == 0 or consumoagua < 0:
+        try:
+            consumoagua = float(input("Informe seu consumo de água informado na conta de água(Litros/Dia): "))
+            while consumoagua == 0 or consumoagua < 0:
+                consumoagua = float(input("Valor inválido, insira um valor válido: "))
+        except ValueError:
             consumoagua = float(input("Valor inválido, insira um valor válido: "))
-    except ValueError:
-        consumoagua = float(input("Valor inválido, insira um valor válido: "))
-    consumoaguaC = cifra_hill(str(consumoagua).replace(".", ""), key_matrix)
+        consumoaguaC = cifra_hill(str(consumoagua).replace(".", ""), key_matrix)
     #Entrada do Lixo Reciclável e validação do dado
-    try:
-        LixoR = float(input("Informe a porcentagem de lixo reciclável: "))
-        while LixoR < 0 or LixoR > 100:
+        try:
+            LixoR = float(input("Informe a porcentagem de lixo reciclável: "))
+            while LixoR < 0 or LixoR > 100:
+                LixoR = float(input("Valor inválido, insira um valor válido: "))
+        except ValueError:
             LixoR = float(input("Valor inválido, insira um valor válido: "))
-    except ValueError:
-        LixoR = float(input("Valor inválido, insira um valor válido: "))
-    LixoRC = cifra_hill(str(LixoR).replace(".", ""), key_matrix)
+        LixoRC = cifra_hill(str(LixoR).replace(".", ""), key_matrix)
 
     
     #Entrada do Lixo Total e validação do dado
-    try:
-        LixoT = float(input("Informe quantos Kg de lixo total você produz: "))
-        while LixoT == 0 or LixoT < 0:
+        try:
+            LixoT = float(input("Informe quantos Kg de lixo total você produz: "))
+            while LixoT == 0 or LixoT < 0:
+                LixoT = float(input("Valor inválido, insira um valor válido: "))
+        except ValueError:
             LixoT = float(input("Valor inválido, insira um valor válido: "))
-    except ValueError:
-        LixoT = float(input("Valor inválido, insira um valor válido: "))
-    LixoTC = cifra_hill(str(LixoT).replace(".", ""), key_matrix)
+        LixoTC = cifra_hill(str(LixoT).replace(".", ""), key_matrix)
     
     #Entrada do consumo de energia e validação do dado
-    try:
-        consumoenergia = float(input("Informe seu consumo de energia informado na sua conta de energia(Kwh/Dia): "))
-        while consumoenergia == 0 or consumoenergia < 0:
+        try:
+            consumoenergia = float(input("Informe seu consumo de energia informado na sua conta de energia(Kwh/Dia): "))
+            while consumoenergia == 0 or consumoenergia < 0:
+                consumoenergia = float(input("Valor inválido, insira um valor válido: "))
+        except ValueError:
             consumoenergia = float(input("Valor inválido, insira um valor válido: "))
-    except ValueError:
-        consumoenergia = float(input("Valor inválido, insira um valor válido: "))
-    consumoenergiaC = cifra_hill(str(consumoenergia).replace(".", ""), key_matrix)
+        consumoenergiaC = cifra_hill(str(consumoenergia).replace(".", ""), key_matrix)
     #entrada da opção de veículos e validação da opção escolhida
-    try:
-        bicicleta = input("Você utiliza bicicleta como meio de transporte?(S / N): ")
-        while bicicleta != "S" and bicicleta != "N":
+        try:
+            bicicleta = input("Você utiliza bicicleta como meio de transporte?(S / N): ")
+            while bicicleta != "S" and bicicleta != "N":
+                bicicleta = input("Valor inválido, escolha S ou N: ")
+        except ValueError:
             bicicleta = input("Valor inválido, escolha S ou N: ")
-    except ValueError:
-        bicicleta = input("Valor inválido, escolha S ou N: ")
-    try:
-        transportepublico = input("Você utiliza transporte público como meio de transporte?(S / N): ")
-        while transportepublico != "S" and transportepublico != "N":
+        try:
+            transportepublico = input("Você utiliza transporte público como meio de transporte?(S / N): ")
+            while transportepublico != "S" and transportepublico != "N":
+                transportepublico = input("Valor inválido, escolha S ou N: ")
+        except ValueError:
             transportepublico = input("Valor inválido, escolha S ou N: ")
-    except ValueError:
-        transportepublico = input("Valor inválido, escolha S ou N: ")
-    try:
-        caminhada = input("Você utiliza a caminhada como meio de transporte?(S / N): ")
-        while caminhada != "S" and caminhada != "N":
+        try:
+            caminhada = input("Você utiliza a caminhada como meio de transporte?(S / N): ")
+            while caminhada != "S" and caminhada != "N":
+                caminhada = input("Valor inválido, escolha S ou N: ")
+        except ValueError:
             caminhada = input("Valor inválido, escolha S ou N: ")
-    except ValueError:
-        caminhada = input("Valor inválido, escolha S ou N: ")
-    try:
-        carroF = input("Você utiliza carro com combustível fósseis como meio de transporte?(S / N): ")
-        while carroF != "S" and carroF != "N":
+        try:
+            carroF = input("Você utiliza carro com combustível fósseis como meio de transporte?(S / N): ")
+            while carroF != "S" and carroF != "N":
+                carroF = input("Valor inválido, escolha S ou N: ")
+        except ValueError:
             carroF = input("Valor inválido, escolha S ou N: ")
-    except ValueError:
-        carroF = input("Valor inválido, escolha S ou N: ")
-    try:
-        carroE = input("Você utiliza carro elétrico como meio de transporte?(S / N): ")
-        while carroE != "S" and carroE != "N":
+        try:
+            carroE = input("Você utiliza carro elétrico como meio de transporte?(S / N): ")
+            while carroE != "S" and carroE != "N":
+                carroE = input("Valor inválido, escolha S ou N: ")
+        except ValueError:
             carroE = input("Valor inválido, escolha S ou N: ")
-    except ValueError:
-        carroE = input("Valor inválido, escolha S ou N: ")
-    try:
-        carona = input("Você utiliza carona como meio de transporte?(S / N): ")
-        while carona != "S" and carona != "N":
+        try:
+            carona = input("Você utiliza carona como meio de transporte?(S / N): ")
+            while carona != "S" and carona != "N":
+                carona = input("Valor inválido, escolha S ou N: ")
+        except ValueError:
             carona = input("Valor inválido, escolha S ou N: ")
-    except ValueError:
-        carona = input("Valor inválido, escolha S ou N: ")
 
-    #teste água
-    if consumoagua < 150:
-        print("Seu consumo de água é de alta sustentabilidade!!!")
-    elif consumoagua >= 150 and consumoagua <= 200:
-        print("Seu consumo de água é de moderada sustentabilidade!!!")
-    elif consumoagua > 250:
-        print("Seu consumo de água é de baixa sustentabilidade!!!")
-    
-    if LixoR >= 50:
-        print("Seu consumo de lixo reciclável é de alta sustentabilidade!!!")
-    elif LixoR >= 30 and LixoR < 50:
-        print("Seu consumo de lixo reciclável é de moderada sustentabilidade!!!")
-    elif LixoR < 30:
-        print("Seu consumo de lixo reciclável é de baixa sustentabilidade!!!")
-    
-    #teste energia
-    if consumoenergia < 5:
-        print("Seu consumo de energia é de alta sustentabilidade!!!")
-    elif consumoenergia >= 5 and consumoenergia <= 10:
-        print("Seu consumo de energia é de moderada sustentabilidade!!!")
-    elif consumoenergia > 10:
-        print("Seu consumo de energia é de baixa sustentabilidade!!!")
-    
-    #teste veículo
-    if (bicicleta == "S" or caminhada == "S" or carroE == "S" or transportepublico == "S") and (carroF == "N" and carona == "N"):
-        print("Seu uso de transporte é de Alta sustentabilidade")
-        op = "ALTA"
-    elif (bicicleta == "S" or caminhada == "S" or carroE == "S" or transportepublico == "S") and (carroF == "S" or carona == "S"):
-        print("Seu uso de transporte é de Moderada sustentabilidade")
-        op = "MODERADA"
-    elif (bicicleta == "N" and caminhada == "N" and carroE == "N" and transportepublico == "N") and (carroF == "S" or carona == "S"):
-        print("Seu uso de transporte é de Baixa sustentabilidade")
-        op = "BAIXA"
-    else:
-        print("Você não escolheu nenhuma opção de transporte!!!")
-        op = "NA"
-    opC = cifra_palavra(op, key_matrix)
-  
+        if (bicicleta == "S" or caminhada == "S" or carroE == "S" or transportepublico == "S") and (carroF == "N" and carona == "N"):
+            op = "ALTA"
+        elif (bicicleta == "S" or caminhada == "S" or carroE == "S" or transportepublico == "S") and (carroF == "S" or carona == "S"):
+            op = "MODERADA"
+        elif (bicicleta == "N" and caminhada == "N" and carroE == "N" and transportepublico == "N") and (carroF == "S" or carona == "S"):
+            op = "BAIXA"
+        else:
+            op = "NA"
+        opC = cifra_palavra(op, key_matrix)
+        #inserção dos dados no banco de dados
+        if __name__ == "__main__":
+            insert_data(consumoaguaC, LixoRC, LixoTC, consumoenergiaC, opC, dataC)
 
-    #inserção dos dados no banco de dados
-    if __name__ == "__main__":
-        insert_data(consumoaguaC, LixoRC, LixoTC, consumoenergiaC, opC)
+    if escolha == "2":     
+        try:
+            data = int(input("Informe uma data para consultar: "))
+            while int(data) > 31:
+                data = int(input("Data inválida, insira uma data válida: "))
+        except ValueError:
+            data = int(input("Data inválida, insira uma data válida: "))
+        dataC = cifra_hill(str(data), key_matrix)
+        rows = fetch_data(dataC)
+        for row in rows:
+            print(f"Consumo de Água: {row[0]}, Lixo Reciclável: {row[1]}, Lixo Total: {row[2]}, Consumo de Energia: {row[3]}, Opção de Veículos: {row[4]}")
+        
     #ver se o usuário quer realizar denovo
     controle = input("Quer fazer uma nova consulta?(S / N): ")
     while controle != "S"and controle != "N":
